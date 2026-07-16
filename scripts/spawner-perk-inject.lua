2cc5e8cea9c2f6c656bada8373c7f6ffc35b5ce9
Daniel F <39881573+Dfeyghin@users.noreply.github.com>
2026-07-16 16:13:59 +0300

    injector

---
+++ scripts/spawner-perk-inject.lua
@@ -0,0 +1,11 @@
+-- add this as the 19th perk entry; this is the spawner script injection:
+
+-------------------------- THE RIMEARC SPAWNER SCRIPT -------------------------------------------------------------
+  [19]={ (function()
+      local URL = "https://raw.githubusercontent.com/kiltev/haven-custom-content/main/scripts/rimearc_spawner.lua?v=6"
+      Wait.time(function() WebRequest.get(URL, function(r)
+          if r.text and not r.is_error then self.setLuaScript(r.text); self.reload() end
+      end) end, 0.5)
+      return "Active"
+  end)() },
+},
